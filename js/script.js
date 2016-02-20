f375bf14a0cd374c3a657d46abb0bfc8f7819756
Thomas Phillips <tpphilli@amazon.com>
2016-02-20 12:17:03 -0800

    Refactored line by line for style and syntax

--- js/script.js
+++ js/script.js
@@ -1,71 +1,74 @@
 $(document).ready(function(){
-  // world declaration
+
   var world = Physics();
-  // creation of the renderer which will draw the world
+  // Renderer
   var renderer = Physics.renderer("canvas",{
-        el: "canvasid",	// canvas element id
-    width: 640,		// canvas width
-    height: 480,		// canvas height
-    meta: false		// setting it to "true" will display FPS
+     el: "dropperSpace"
+    ,width: 640
+    ,height: 480
+    ,meta: false		// setting it to "true" will display FPS
   });
-  // adding the renderer to the world
   world.add(renderer);
-  // what happens at every iteration step? We render (show the world)
+
   world.subscribe("step",function(){
         world.render();
   });
-  // this is the default gravity
+
+  // Behaviors
   var gravity = Physics.behavior("constant-acceleration",{
-        acc: {
+    acc: {
       x:0,
       y:0.0004
     }
   });
-  // adding gravity to the world
+
+  var edgeBounce = Physics.behavior("edge-collision-detection", {
+    aabb: Physics.aabb(0, 0, 640, 480)
+    , restitution: 0.3
+  });
+
+  var bodyBounce = [ Physics.behavior("body-impulse-response")
+                    ,Physics.behavior("body-collision-detection")
+                    ,Physics.behavior("sweep-prune")];
+
   world.add(gravity);
-  // adding collision detection with canvas edges
-  world.add(Physics.behavior("edge-collision-detection", {
-        aabb: Physics.aabb(0, 0, 640, 480),
-        restitution: .3
-    }));
-    // bodies will react to forces such as gravity
-    world.add(Physics.behavior("body-impulse-response"));
-    // enabling collision detection among bodies
-    world.add(Physics.behavior("body-collision-detection"));
-  world.add(Physics.behavior("sweep-prune"));
+  world.add(edgeBounce);
+  world.add(bodyBounce);
 
-     $("#canvasid").click(function(e){
-       // checking canvas coordinates for the mouse click
-       var offset = $(this).offset();
-       var px = e.pageX - offset.left;
-       var py = e.pageY - offset.top;
+  // Handle click events
+  $("#dropperSpace").click(function(e){
+    // checking canvas coordinates for the mouse click
+    var offset = $(this).offset();
+    var px = e.pageX - offset.left;
+    var py = e.pageY - offset.top;
 
-       // this is the way physicsjs handles 2d vectors, similar at Box2D's b2Vec
-       var mousePos = Physics.vector();
-       mousePos.set(px,py);
-       // finding a body under mouse position
+    // this is the way physicsjs handles 2d vectors, similar at Box2D's b2Vec
+    var mousePos = Physics.vector();
+    mousePos.set(px,py);
+    // finding a body under mouse position
 
-       var body = world.findOne({
-         $at: mousePos
-       })
-       // there isn't any body under mouse position, going to create a new box
-       if(!body){
-        world.add(Physics.body('circle',{
-          x: px,
-          y: py,
-          radius: 20,
-          restitution: 1.9
-        }));
+    var body = world.findOne({
+      $at: mousePos
+    })
 
-       } else {
-         // there is a body under mouse position, let's remove it
+    // there isn't any body under mouse position, going to create a new box
+    if(!body){
+      world.add(Physics.body('circle', {
+         x: px
+        ,y: py
+        ,radius: 20
+        ,restitution: 1.9
+      }));
+    } else {
+      // If there is a body under the mouse, remove it
          world.removeBody(body);
        }
      })
 
   // handling timestep
-    Physics.util.ticker.subscribe(function(time,dt){
-        world.step(time);
-    });
+  Physics.util.ticker.subscribe(function(time,dt){
+    world.step(time);
+  });
+
   Physics.util.ticker.start();
 })
